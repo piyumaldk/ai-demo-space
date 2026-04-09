e11f56f50dd53b85121b05ad661e4a8e03d4ca93
piyumaldk <piyumaldk@gmail.com>
2026-04-09 16:07:40 +0530

    Remove BFF from default

--- react-vite/src/config/config.ts
+++ react-vite/src/config/config.ts
@@ -1,4 +1,4 @@
 import { getEnvOrDefault } from "../utils/getEnvOrDefault";
 
-export const BFF_BASE = getEnvOrDefault("VITE_BFF_URL", "http://localhost:8000");
+export const BFF_BASE = getEnvOrDefault("VITE_BFF_URL", "");
 export const GOOGLE_CLIENT_ID = getEnvOrDefault("VITE_GOOGLE_CLIENT_ID", "");
